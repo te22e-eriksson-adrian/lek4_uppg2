a159a9f910c8690967e435b631338cbe9b8c6e85
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-13 10:08:02 +0200

    gjort så att man kan skriva text och räkna tecken

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Skriva en text
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.println("Skriv in en text:");
+        String text = tangentbord.nextLine();
+
+        //Visa antalet ord
+        int antal_tecken = text.length();
+        System.out.println("Texten du skrev ovan innehåller "+antal_tecken+" tecken.");
+        tangentbord.close();
     }
 }
