aeebeab3f0db8ee90854d071b21a97a560601ef2
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-13 10:17:51 +0200

    del b - hitta bokstav klar

--- src/App.java
+++ src/App.java
@@ -8,8 +8,16 @@ public class App {
         String text = tangentbord.nextLine();
 
         //Visa antalet ord
+        System.out.println("        ");
         int antal_tecken = text.length();
         System.out.println("Texten du skrev ovan innehåller "+antal_tecken+" tecken.");
+
+        //Söka efter tecken som skrivits tidigare
+        System.out.println("        ");
+        System.out.print("Ange tecken att söka efter: ");
+        String text2 = tangentbord.nextLine();
+        int t_index = text.indexOf(text2);
+        System.out.println("Tecknet befinner sig på plats nummer "+t_index);
         tangentbord.close();
     }
 }
